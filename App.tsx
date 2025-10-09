f67f4ee4d1a349b9f2dccd50bce66a1992501181
Daniel <danielghierro@gmail.com>
2025-10-09 09:32:03 +0200

    PruebaBotones

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable,StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+
+   const[count,setcount] = useState(10)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textHuge}>{count}</Text>
+
+      <Pressable onPress={() => setcount(count + 1)} >
+        <Text style={styles.floatingButton}>+1</Text>
+      </Pressable>
+
+      <TouchableOpacity>
+        <Text>+1</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +27,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+
+
+  textHuge:{
+fontSize:120,
+fontWeight:'thin',
+
+  },
+  
+  floatingButton:{
+ position: 'absolute',
+ bottom: -200,
+ right: -200,
+ backgroundColor: '#65558F',
+ padding: 20,
+ borderRadius: 15,
+ shadowColor: '#000',
+ shadowOffset: { width: 0, height: 4},
+
+
+
+}});
