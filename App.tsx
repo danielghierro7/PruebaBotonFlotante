7ae10e51dd29ced9f08062928254b6997a43b3b1
Daniel <danielghierro@gmail.com>
2025-10-17 09:02:15 +0200

    Inicio

--- App.tsx
+++ App.tsx
@@ -1,24 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { Pressable,StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useState } from 'react';
+import FAB from './components/FAB';
 
 export default function App() {
 
 
    const[count,setcount] = useState(10)
   return (
-    <View style={styles.container}>
-      <Text style={styles.textHuge}>{count}</Text>
+<View style={styles.container}>
+  <Text style={styles.textHuge}>{count}</Text>
 
-      <Pressable style={styles.topLeft} onPress={() => setcount(count + 1)} >
-        <Text >+1</Text>
-      </Pressable>
+  {/* Comentado: Pressable para incrementar el contador */}
+  {/*
+  <Pressable style={styles.topLeft} onPress={() => setcount(count + 1)} >
+    <Text >+1</Text>
+  </Pressable>
+  */}
 
-      <TouchableOpacity>
-        <Text>+1</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+  {/*
+  <TouchableOpacity>
+    <Text>+1</Text>
+  </TouchableOpacity>
+  */}
+  <FAB label= "Hola soy Dani Hierro" onPress={()=> setcount(count+1)} onLongPress={()=> setcount(0)} position='left' />
+  <StatusBar style='auto' />
+</View>
   );
 }
 
@@ -27,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
 
   },
 
--- components/FAB.tsx
+++ components/FAB.tsx
@@ -1,27 +1,49 @@
- import React, { Component } from 'react'
- import { Pressable, Text, View } from 'react-native'
- 
+ import { useState } from 'react';
+import { View, Text,Pressable,StyleSheet } from 'react-native'
 
-interface FABProps {
-    label:string;
-    color:string;
+interface Props {
+ label: string;
+ onPress: () => void;
+ onLongPress: () => void;
+  position?: 'left' | 'right';
 }
 
- const FAB:React.FC<FABProps> = ({label,color}) => {
-return(
- <View>
-    <Pressable style={{backgroundColor: color}}>
-        <Text>
-            {label}
-        </Text>
-    </Pressable>
- </View>
 
 
-);
+export default function FAB({label,onPress,onLongPress,position="right"}:Props){
 
-
-
- };
- export default FAB;
+    const[count,setCount]=useState(0);
+ return (
  
+ 
+ <Pressable
+ style={[styles.floatingButton ,styles.right]}
+ onPress={onPress}
+ onLongPress={onLongPress}
+ >
+ <Text style={{ color: "white", fontSize: 20 }}>{label} </Text>
+     </Pressable>
+
+)}
+
+
+     const styles = StyleSheet.create({
+ floatingButton: {
+ position: 'absolute',
+
+ backgroundColor: '#65558F',
+ padding: 20,
+ borderRadius: 15,
+ shadowColor: '#000',
+ shadowOffset: { width: 0, height: 4},
+ shadowOpacity: 0.3,
+ elevation: 3,
+ shadowRadius: 4,
+ },
+
+
+ right:{
+    right:30,
+    bottom:0,
+ }
+});
